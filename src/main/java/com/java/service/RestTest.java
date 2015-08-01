26fe697e45c56ceaf1f37cdfc80e8d5e57e48bd4
uSf <usf.alami@gmail.com>
2015-08-01 16:23:24 +0200

    init rest

---
+++ src/main/java/com/java/service/RestTest.java
@@ -0,0 +1,13 @@
+package com.java.service;
+
+
+//@Path("./test")
+public class RestTest {
+	
+//	@GET
+//	@Produces(MediaType.TEXT_HTML)
+	public String getTest(){
+		return "<h1>Rest well setup</h1>";
+	}
+
+}
