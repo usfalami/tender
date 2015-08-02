3453cac517297cba1bee3096353dfe6d9916294e
Zied.HLEL <ziad.hlel@gmail.com>
2015-08-02 19:03:24 +0200

    subscription_Bean

--- src/main/java/com/java/bean/Authentification.java
+++
@@ -1,5 +0,0 @@
-package com.java.bean;
-
-public class Authentification {
-
-}
---
+++ src/main/java/com/tender/java/bean/Contact.java
@@ -0,0 +1,70 @@
+package com.tender.java.bean;
+
+public class Contact {
+
+	private Actor actor;
+	private User user;
+	private long id;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String fonction;
+	private String service;
+	
+	
+	public String getService() {
+		return service;
+	}
+
+	public void setService(String service) {
+		this.service = service;
+	}
+
+	public Actor getActor() {
+		return actor;
+	}
+
+	public void setActor(Actor actor) {
+		this.actor = actor;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getFonction() {
+		return fonction;
+	}
+
+	public void setFonction(String fonction) {
+		this.fonction = fonction;
+	}
+}
