c3066b7d2b9755613d028569eb042abbbe08d2d0
jihed <jied@hotmail.com>
2015-08-02 19:24:09 +0200

    bean build

--- src/main/java/com/java/bean/Tender.java
+++ src/main/java/com/java/bean/Tender.java
@@ -1,12 +1,16 @@
 package com.java.bean;
 
+import java.io.File;
 import java.util.Date;
+import java.util.List;
 
 public class Tender extends Action{
 	
 	private String title;
 	private Date publicationDate;
 	private Date responseDeadLine;
-	protected Adress adress;
+	protected Adress executionAdress;
+    private List<File> documents;
+    private String description;
 
 }
