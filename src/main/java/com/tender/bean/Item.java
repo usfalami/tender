58bb8c9027093e8542993f45ba1ebeae3ac23897
uSf <usf.alami@gmail.com>
2015-08-09 01:46:03 +0200

    add units

--- src/main/java/com/tender/bean/Item.java
+++ src/main/java/com/tender/bean/Item.java
@@ -1,7 +1,27 @@
 package com.tender.bean;
 
+import com.tender.bean.unit.Unit;
+import com.tender.bean.unit.Unit.CAPACITY;
+import com.tender.bean.unit.Unit.DATA;
+import com.tender.bean.unit.Unit.ENERGY;
+import com.tender.bean.unit.Unit.VOLUME;
+import com.tender.bean.unit.Unit.WEIGHT;
+
 public class Item {
 
 	protected String name;
+	protected Unit<?> unit; // = new Unit<LENGTH>() // = new Unit<WEIGHT>(); 
+
+	public static void main(String[] args) {
+		// unit can be :
+		
+		Unit<?> a = null;
+		System.out.println(a = new Unit<WEIGHT>(WEIGHT.KG));
+		System.out.println(a = new Unit<DATA>(DATA.MO));
+		System.out.println(a = new Unit<VOLUME>(VOLUME.M3));
+		System.out.println(a = new Unit<ENERGY>(ENERGY.W));
+		System.out.println(a = new Unit<CAPACITY>(CAPACITY.ML));
+	}
+	
 	
 }
---
+++ src/main/java/com/tender/bean/unit/Unit.java
@@ -0,0 +1,55 @@
+package com.tender.bean.unit;
+
+
+public class Unit<T extends Enum<?>> { // force Enum<?> implements Mesure !?
+
+	private T value;
+	
+	public Unit() {
+		
+	}	
+	public Unit(T value) {
+		this.value = value;
+	}
+
+	public void setValue(T value) {
+		this.value = value;
+	}
+	public T getValue() {
+		return value;
+	}
+	
+	public long toReelValue() {
+		return (long) Math.pow(10, value.ordinal());
+	}
+	@Override
+	public String toString() {
+		return "Unit [value=" + value + "]";
+	}
+	
+	
+	public static enum LENGTH implements Mesure {
+		MM, CM, DM, M, DAM, HM, KM;
+	}
+	public static enum AREA implements Mesure {
+		MM2, CM2, DM2, M2, DAM2, HM2, KM2;
+	}
+	public static enum VOLUME implements Mesure {
+		MM3, CM3, DM3, M3, DAM3, HM3, KM3;
+	}
+	public static enum WEIGHT implements Mesure {
+		MG, CG, DG, G, DAG, HG, KG, KGx10, Q, T;
+	}
+	public static enum CAPACITY implements Mesure  {
+		ML, CL, DL, L, DAL, HL;
+	}
+	public static enum DATA implements Mesure {
+		B, O, KO, MO, GO, TO, Kio, Mio, Gio, Tio;
+	}
+	public static enum ENERGY implements Mesure {
+		W, KW; //.....
+	}
+	
+	public static interface Mesure {}
+	
+}
